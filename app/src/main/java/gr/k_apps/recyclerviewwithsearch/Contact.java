f8f667e3bddfd97b697fcc4bf3aefe38210abdfc
konbak <konbak19@gmail.com>
2018-01-13 14:18:22 +0200

    Initial commit

---
+++ app/src/main/java/gr/k_apps/recyclerviewwithsearch/Contact.java
@@ -0,0 +1,26 @@
+package gr.k_apps.recyclerviewwithsearch;
+
+/**
+ * Created by konbak on 13/1/2018.
+ */
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
